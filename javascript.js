171a6adc82b6bf812a18125c49a16bac35e0afb3
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-01-13 07:15:52 +0100

    Created the function to obtain computer choice

--- javascript.js
+++ javascript.js
@@ -4,3 +4,15 @@ const computerScore = 0;
 const humanScore = 0; 
 playRound = 1; 
 
+// Logic to get computer choice 
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    
+    // Generate a random index between 0 and 2
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    
+    // Return the random choice
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
